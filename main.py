32fd69490a139dcbc905c1d4307a42c4c77aec11
Alvaro759 <acargar730@g.educaand.es>
2023-11-13 10:37:42 +0100

    Agregar funciones al menu

---
+++ main.py
@@ -0,0 +1,6 @@
+import menu
+
+menu.mostrar_menu()  # Mostrar el menú
+
+opcion = menu.obtener_numero()  # Obtener la opción del usuario
+print(f"Has seleccionado la opción {opcion}")
---
+++ menu.py
@@ -0,0 +1,10 @@
+def mostrar_menu():
+    print("Menú:")
+    print("1- Sumar")
+    print("2- Restar")
+    print("3- Multiplicar")
+    print("4- Dividir")
+    print("5- Salir")
+
+def obtener_numero():
+    return int(input("Ingrese un número: "))
