3f3f7d6cdbe40c2fcf90443a8ae92089cbd36969
Alvaro759 <acargar730@g.educaand.es>
2023-11-13 10:46:26 +0100

    Añadir funcion dividir a operaciones.py

--- operaciones.py
+++ operaciones.py
@@ -21,3 +21,26 @@ def multiplicar(a, b):
         return resultado
     else:
         raise ValueError("Ambos parámetros deben ser números enteros o decimales.")
+
+
+def dividir(valor1, valor2):
+    """
+    Divide dos valores y retorna el resultado.
+    """
+    # Verificar que los valores sean int o float
+    if not (isinstance(valor1, (int, float)) and isinstance(valor2, (int, float))):
+        raise ValueError("Ambos valores deben ser de tipo int o float")
+
+    # Verificar que el divisor no sea cero
+    if valor2 == 0:
+        raise ValueError("El divisor no puede ser cero")
+
+    # Inicializar el resultado
+    resultado = 0
+
+    # Restar iterativamente valor2 a valor1 hasta que valor1 sea menor que valor2
+    while valor1 >= valor2:
+        valor1 -= valor2
+        resultado += 1
+
+    return resultado
